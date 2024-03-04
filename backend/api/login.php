270b42094ead8b7d07c13d231182dcd3ecf5d517
Thanawat Sonsanoy <118097228+jpjapan@users.noreply.github.com>
2024-03-04 21:30:05 +0700

    update api
    login
    response : on localstorage
    uid, username

---
+++ backend/api/login.php
@@ -0,0 +1,37 @@
+<?php
+require_once('./config.php');
+
+if ($_SERVER['REQUEST_METHOD'] == "POST") {
+    //รับค่าจากหน้าบ้านมา
+    $txt_username = $_POST['username'];
+    $txt_password = $_POST['password'];
+
+    $query = "select * from users where id = ?";
+    $stmt = $conn->prepare($query);
+    if ($stmt->execute([$txt_username])) {
+        $num = $stmt->rowCount();
+        if ($num == 1) {
+            $row = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($txt_password === $row['password']) {
+                $object = new stdClass();
+                $object->RespCode = 200;
+                $object->RespMessage = 'loginsuccess';
+                $object->RespUsername = $row['id'];
+                $object->RespUid = $row['uid'];
+            } else {
+                $object = new stdClass();
+                $object->RespCode = 400;
+                $object->RespMessage = 'invalidpassword';
+                $object->Log = 1;
+            }
+            echo json_encode($object);
+        }
+    } else {
+        $object = new stdClass();
+        $object->RespCode = 450;
+        $object->RespMessage = 'usernameisnotexist';
+        $object->Log = 2;
+    }
+} else {
+    http_response_code(405);
+}
